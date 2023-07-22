39fc54e33c25421f6e7f1c7d07fece1aea4cc6e4
Bálint Szekeres <szekeres@b4lint.hu>
2023-07-22 09:48:43 +0200

    compare multiple examples

--- encode.sh
+++ encode.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-for i in {0..63}
+for original in example1/original.jpg example2/original.jpg example3/original.jpg example4/original.png
 do
-	avifenc -j all --min 0 --max 63 --minalpha 0 --maxalpha 63 -a end-usage=q -a cq-level=$i -a tune=ssim original.jpg quality-$i.avif
+	for i in {0..63}
+	do
+		avifenc -j all --min 0 --max 63 --minalpha 0 --maxalpha 63 -a end-usage=q -a cq-level=$i -a tune=ssim $original ${original//\/*/}/q-$i.avif
+	done
 done
